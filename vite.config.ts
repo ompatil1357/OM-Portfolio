6beef4a3245e8eaffdba0623edc1c0dd32907f0d
ompatil1357 <op394907@gmail.com>
2025-12-31 10:12:27 +0530

    fix-deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_URL || "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
